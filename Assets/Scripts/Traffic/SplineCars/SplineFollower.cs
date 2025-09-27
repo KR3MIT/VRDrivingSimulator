63b7ffa67b4d0054c5e5ff3ac19959d3269f8394
Mathias Kofoed Hansen <145567298+MatteThisen@users.noreply.github.com>
2025-09-27 19:46:58 +0200

    Some more spline work tihi

--- Assets/Scripts/Traffic/SplineCars/SplineFollower.cs
+++ Assets/Scripts/Traffic/SplineCars/SplineFollower.cs
@@ -7,7 +7,18 @@ public class SplineFollower : MonoBehaviour
 {
 
     [SerializeField] private List<SplineContainer> splines;
-    int currentSpline = 0;
+
+    int currentSpline = -1;
+    SplineInfo currentSplineInfo;
+    float currentSplineSpeed;
+    SplineInfo nextSplineInfo;
+    float nextSplineSpeed;
+    Vector3 nextSplineStartPoint;
+    bool onLastSpline = false;
+    bool nextSplineOpen;
+
+
+
     SplineAnimate splineAnimate;
 
 
@@ -27,19 +38,56 @@ public class SplineFollower : MonoBehaviour
             StartCoroutine(SwitchSpline());
 
         }
+        AdjustCarSpeed();
     }
 
 
     IEnumerator SwitchSpline()
     {
+        if (onLastSpline) Destroy(gameObject);
+
         currentSpline++;
         splineAnimate.Container = splines[currentSpline];
+        currentSplineInfo = splines[currentSpline].GetComponent<SplineInfo>();
+        
+        if (currentSpline < splines.Count - 1)
+        {
+            nextSplineInfo = splines[currentSpline + 1].GetComponent<SplineInfo>();
+            nextSplineSpeed = nextSplineInfo.TraversalSpeed;
+            nextSplineStartPoint = nextSplineInfo.SplineStartPoint;
+        }
+        else
+        {
+            onLastSpline = true;
+        }
+        AdjustCarSpeed();
+       
+
         splineAnimate.Play();
         yield return new WaitForSeconds(0.01f);
-        //splineAnimate.Pause();
-        //splineAnimate.Restart(false);
+        splineAnimate.Pause();
+        splineAnimate.Restart(true);
 
     }
 
+    private void AdjustCarSpeed()
+    {
+        float distance = Vector3.Distance(transform.position, nextSplineStartPoint);
+
+
+        if (distance < 10f && !onLastSpline)
+        {
+            Debug.Log(transform.position);
+            Debug.Log($"Distance to next spline start: {distance}");
+            float t = 1f - (distance / 10f);
+            Debug.Log($"Adjusting speed with t={t}");
+            float adjustedSpeed = Mathf.Lerp(currentSplineInfo.TraversalSpeed, nextSplineSpeed, t);
+            splineAnimate.MaxSpeed = adjustedSpeed;
+        }
+        else
+        {
+            splineAnimate.MaxSpeed = currentSplineInfo.TraversalSpeed;
+        }
+    }
 
 }
--- Assets/Scripts/Traffic/SplineCars/SplineInfo.cs
+++ Assets/Scripts/Traffic/SplineCars/SplineInfo.cs
@@ -5,11 +5,22 @@ public class SplineInfo : MonoBehaviour
 {
 
     [SerializeField] private float traversalSpeed;
+    private Vector3 splineStartPoint;
+    private SplineContainer splineContainer;
+
+
+    // Expose read-only properties for SplineFollower to access
+    public float TraversalSpeed => traversalSpeed;
+    public Vector3 SplineStartPoint => splineStartPoint;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        splineContainer = GetComponent<SplineContainer>();
+
+        splineStartPoint = splineContainer.Spline.EvaluatePosition(0f);
     }
 
     // Update is called once per frame
