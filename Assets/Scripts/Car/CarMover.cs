8ebd46819e9a61e68e6e8d1512511b8df11569ae
m4tt <mattiasehlert@gmail.com>
2025-09-29 13:49:21 +0200

    car

--- Assets/Scripts/Car/CarMover.cs
+++ Assets/Scripts/Car/CarMover.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 
 public class CarMover : MonoBehaviour
 {
+    //todo: automatic transmission, braking behaviour, engine power curve, 
+
     public enum Axel
     {
         Front,
@@ -22,24 +24,45 @@ public class CarMover : MonoBehaviour
         public Axel axel;
     }
 
+    public enum TransmissionType
+    {
+        Park,
+        Reverse,
+        Neutral,
+        Drive,
+    }
+
+    [Header("Gear/Transmission")]
+    public TransmissionType transmissionState = TransmissionType.Park;
+
+    [Header("Speeds")]
     public float maxAcceleration = 30.0f;
+    public float reverseAcceleration = 20.0f;
     public float brakeAcceleration = 50.0f;
 
+    [Header("Steering")]
     public float turnSensitivity = 1.0f;
     public float maxSteerAngle = 30.0f;
 
+    [Header("Rigidbody")]
     public Vector3 _centerOfMass;
 
+    [Header("Wheels")]
     public List<Wheel> wheels;
 
-    float moveInput;
-    float steerInput;
+    [Header("Input values for debug")]
+    public float acceleratorInput;
+    public float brakeInput;
+    public float steerInput;
 
+    //inputs
     private PlayerInput input;
-    private InputAction gasPedal;
+    private InputAction acceleratorPedal;
     private InputAction brakePedal;
     private InputAction steer;
-    private InputAction clutchPedal;
+
+    private InputAction shiftUp;
+    private InputAction shiftDown;
 
     private Rigidbody carRb;
 
@@ -50,26 +73,45 @@ public class CarMover : MonoBehaviour
 
         input = GetComponent<PlayerInput>();
 
-        gasPedal = input.actions["GasPedal"];
+        acceleratorPedal = input.actions["GasPedal"];
         brakePedal = input.actions["BrakePedal"];
         steer = input.actions["Steer"];
-        clutchPedal = input.actions["Clutch"];
 
-        gasPedal.Enable();
+        acceleratorPedal.Enable();
         brakePedal.Enable();
         steer.Enable();
-        clutchPedal.Enable();
+
+        shiftUp = input.actions["ShiftUp"];
+        shiftDown = input.actions["ShiftDown"];
+
+        shiftUp.Enable();
+        shiftDown.Enable();
+
+
 
         foreach (var wheel in wheels)
         {
             wheel.wheelCollider.ConfigureVehicleSubsteps(5f, 12, 15);
         }
+
+        Invoke(nameof(FreezePositionY), 3f);
+    }
+
+    void FreezePositionY()
+    {
+        carRb.constraints = RigidbodyConstraints.FreezePositionY;
     }
 
     void Update()
     {
         GetInputs();
+        HandleTransmission();
         AnimateWheels();
+
+        if (shiftUp.triggered || shiftDown.triggered)
+        {
+            Debug.Log("Transmission: " + transmissionState);
+        }
     }
 
     void FixedUpdate()
@@ -81,16 +123,53 @@ public class CarMover : MonoBehaviour
 
     void GetInputs()
     {
-        moveInput = gasPedal.ReadValue<float>() - brakePedal.ReadValue<float>();
+        acceleratorInput = acceleratorPedal.ReadValue<float>();
         steerInput = steer.ReadValue<float>();
+        brakeInput = brakePedal.ReadValue<float>();
+
     }
 
     void Move()
     {
+        float torque = 0f;
+
+        if (transmissionState == TransmissionType.Drive)
+        {
+            torque = acceleratorInput * maxAcceleration;
+        }
+        else if (transmissionState == TransmissionType.Reverse)
+        {
+            torque = -acceleratorInput * reverseAcceleration;
+        }//neutral and park = 0 torque
+
         foreach (var wheel in wheels)
         {
-            wheel.wheelCollider.motorTorque = moveInput * maxAcceleration;
+            wheel.wheelCollider.motorTorque = torque;
         }
+
+        Debug.Log("Motor Torque: " + torque);
+    }
+
+    void Brake()
+    {
+        float brakeForce = 0f;
+
+        //if in park should brake hard always
+        if (transmissionState == TransmissionType.Park)
+        {
+            brakeForce = 10000f;
+        }
+        else
+        {
+            brakeForce = brakeInput * brakeAcceleration;
+        }
+
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.brakeTorque = brakeForce;
+        }
+
+        Debug.Log("Brake Force: " + brakeForce);
     }
 
     void Steer()
@@ -105,20 +184,29 @@ public class CarMover : MonoBehaviour
         }
     }
 
-    void Brake()
+    void HandleTransmission()
     {
-        if (clutchPedal.ReadValue<float>() > .95f || moveInput == 0)
-        {
-            foreach (var wheel in wheels)
+        float speed = carRb.linearVelocity.magnitude;
+
+        if (brakeInput > .8f)
+        {//only shift if brake is held
+            if (shiftUp.triggered)
             {
-                wheel.wheelCollider.brakeTorque = brakeAcceleration;
+                if (transmissionState == TransmissionType.Park)
+                    transmissionState = TransmissionType.Reverse;
+                else if (transmissionState == TransmissionType.Reverse && speed < .5f)
+                    transmissionState = TransmissionType.Neutral;
+                else if (transmissionState == TransmissionType.Neutral && speed < .5f)
+                    transmissionState = TransmissionType.Drive;
             }
-        }
-        else
-        {
-            foreach (var wheel in wheels)
+            else if (shiftDown.triggered)
             {
-                wheel.wheelCollider.brakeTorque = 0;
+                if (transmissionState == TransmissionType.Drive && speed < .5f)
+                    transmissionState = TransmissionType.Neutral;
+                else if (transmissionState == TransmissionType.Neutral && speed < .5f)
+                    transmissionState = TransmissionType.Reverse;
+                else if (transmissionState == TransmissionType.Reverse)
+                    transmissionState = TransmissionType.Park;
             }
         }
     }
@@ -134,4 +222,22 @@ public class CarMover : MonoBehaviour
             wheel.wheelModel.transform.rotation = rot;
         }
     }
+
+    //void Brake()
+    //{
+    //    if (driftPedal.ReadValue<float>() > .95f || moveInput == 0)
+    //    {
+    //        foreach (var wheel in wheels)
+    //        {
+    //            wheel.wheelCollider.brakeTorque = brakeAcceleration;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        foreach (var wheel in wheels)
+    //        {
+    //            wheel.wheelCollider.brakeTorque = 0;
+    //        }
+    //    }
+    //}
 }
