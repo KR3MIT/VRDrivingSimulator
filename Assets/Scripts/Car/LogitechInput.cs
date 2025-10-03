6c4372f322507f992422a6e678017a45177e1a70
Victor Andersen <akaliasthegamer@gmail.com>
2025-10-03 11:00:08 +0200

    UI and input stuff 😎

--- Assets/Scripts/Car/LogitechInput.cs
+++ Assets/Scripts/Car/LogitechInput.cs
@@ -12,6 +12,11 @@ public class LogitechInput : MonoBehaviour
     public bool leftBlinker;
     public bool rightBlinker;
 
+    public bool SelectButtonA;
+    public bool StartButton;
+
+    public int dpadValue; // center = -1, up = 0, right = 2, down = 3, left = 4
+
     void Start()
     {
         LogitechGSDK.LogiSteeringInitialize(false);
@@ -29,6 +34,8 @@ public class LogitechInput : MonoBehaviour
             speederValue = rec.lY;
             brakeValue = rec.lRz;
 
+            dpadValue = (int)rec.rgdwPOV[0];
+
             if (rec.rgbButtons[4] == 128)
             {
                 rightBlinker = true;
@@ -42,6 +49,32 @@ public class LogitechInput : MonoBehaviour
             else { leftBlinker = false;}
 
 
+            if (rec.rgbButtons[0] == 128)
+            {
+                SelectButtonA = true;
+            }
+            else { SelectButtonA = false; }
+
+            if (rec.rgbButtons[6] == 128)
+            {
+                StartButton = true;
+            }
+            else { StartButton = false; }
+
+
+
+            switch (rec.rgdwPOV[0])
+            {
+                case (0): dpadValue = 0; break; //up
+                //case (4500): actualState += "POV : UP-RIGHT\n"; break; //up-right
+                //case (9000): actualState += "POV : RIGHT\n"; break; //right
+                //case (13500): actualState += "POV : DOWN-RIGHT\n"; break; //down-right
+                case (18000): dpadValue += 1; break; //down
+                //case (22500): actualState += "POV : DOWN-LEFT\n"; break; //down-left
+                //case (27000): actualState += "POV : LEFT\n"; break; //left
+                //case (31500): actualState += "POV : UP-LEFT\n"; break; //up-left
+                default: dpadValue += -1; break; //center
+            }
         }
     }
 
--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,16 +1,95 @@
+using Logitech;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject mainMenu;
+    public GameObject startMenu;
+    public GameObject controlsMenu;
+
+    public Button[] menuButtons;
+
+    private int selectedButtonIndex = 0;
+    private LogitechInput logitechInput;
+    private float inputDelay = 0.2f; // Delay between input reads
+    private float lastInputTime;
+
+    private void Start()
     {
-        
+        logitechInput = GetComponent<LogitechInput>();
+        SelectButton(selectedButtonIndex);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleSteeringWheelInput();
     }
+
+    void HandleSteeringWheelInput()
+    {
+       
+        if (Time.time - lastInputTime > inputDelay)
+        {
+            if (logitechInput.dpadValue == 0) // Up
+            {
+                selectedButtonIndex = (selectedButtonIndex - 1 + menuButtons.Length) % menuButtons.Length;
+                SelectButton(selectedButtonIndex);
+                lastInputTime = Time.time;
+            }
+            else if (logitechInput.dpadValue == 1) // Down
+            {
+                selectedButtonIndex = (selectedButtonIndex + 1) % menuButtons.Length;
+                SelectButton(selectedButtonIndex);
+                lastInputTime = Time.time;
+            }
+        }
+
+        // A button
+        if (logitechInput.SelectButtonA)
+        {
+            menuButtons[selectedButtonIndex].onClick.Invoke();
+        }
+    }
+
+
+    void SelectButton(int index)
+    {
+        EventSystem.current.SetSelectedGameObject(menuButtons[index].gameObject);
+    }
+
+
+    public void StartButton()
+    {
+        mainMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+
+    public void ControlsButton()
+    {
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+    }
+
+
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        startMenu.SetActive(false);
+        controlsMenu.SetActive(false);
+    }
+
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    //StartMenu Scenario Buttons
+    
+
+
+
 }
