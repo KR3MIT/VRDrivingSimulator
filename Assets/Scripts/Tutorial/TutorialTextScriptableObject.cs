ec6aee398236b1fc9ab420a5129620a4fc117266
Oskar B. Kaare <oskar.kaare@gmail.com>
2025-10-03 13:53:36 +0200

    updated tutorials

---
+++ Assets/Scripts/Tutorial/TutorialTextScriptableObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TutorialText", menuName = "ScriptableObjects/TutorialTextScriptableObject", order = 1)]
+public class TutorialTextScriptableObject : ScriptableObject
+{
+    public string LeftTurnTask;
+    public string LeftTurnStep1;
+    public string LeftTurnStep2;
+    public string LeftTurnDone;
+    public string LeftBlinkerTask;
+    public string LeftBlinkerStep1;
+
+}
