756fdaeb03563de176e098bfc131bd6fafb3d0c2
William Abildtrup <william@abildtrup.eu>
2025-10-06 10:42:50 +0200

    light traffic. now skewed from each other

--- Assets/Scripts/Traffic/TrafficLightManager.cs
+++ Assets/Scripts/Traffic/TrafficLightManager.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class TrafficLightManager : MonoBehaviour
 {
-    public enum TrafficLightState { Red, RedYellow, Green, Yellow}
+    public enum TrafficLightState { Green, Yellow, Red, RedYellow}
+
+    [Header("Initial States")]
     public TrafficLightState currentState1 = TrafficLightState.Red;
     public TrafficLightState currentState2 = TrafficLightState.Green;
+
 
     public List<GameObject> trafficLights1 = new List<GameObject>();
     public List<GameObject> trafficLights2 = new List<GameObject>();
 
-    List<Material> materialsLights1 = new List<Material>();
-    List<Material> materialsLights2 = new List<Material>();
+    List<Material> mats1 = new List<Material>();
+    List<Material> mats2 = new List<Material>();
 
-    public float redWait = 24f;
-    public float redYellowWait = 2f;
-    public float greenWait = 17f;
-    public float yellowWait = 5f;
+    [Header("State Durations")]
+    public float redTime = 24f;
+    public float redYellowTime = 1.5f;
+    public float greenTime = 17f;
+    public float yellowTime = 3f;
+
+    private float cycleTimer = 0f;
+    private float totalCycle;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(LightStateSwitch(currentState1));
-        StartCoroutine(LightStateSwitch(currentState2));
+       // StartCoroutine(LightStateSwitch(1));
+        //StartCoroutine(LightStateSwitch(2));
+        //StartCoroutine(CoordinatedLightStateSwitch());
         GetMaterials();
         UpdateMaterialState();
+        // compute full cycle length (one light’s total duration)
+        totalCycle = redTime + redYellowTime + greenTime + yellowTime;
+        StartCoroutine(CycleClock());
     }
 
     // Update is called once per frame
@@ -35,38 +46,70 @@ public class TrafficLightManager : MonoBehaviour
         
     }
 
-
-   IEnumerator LightStateSwitch (TrafficLightState currentState)
+    IEnumerator CycleClock()
     {
-        float waitTime = 0;
         while (true)
         {
-            
-            if (currentState == TrafficLightState.Red)
-            {
-                waitTime = redWait;
-            }
-            else if(currentState == TrafficLightState.RedYellow)
-            {
-                waitTime = redYellowWait;
-            }
-            else if (currentState == TrafficLightState.Green)
-            {
-                waitTime = greenWait;
-            }
-            else if (currentState == TrafficLightState.Yellow)
-            {
-                waitTime = yellowWait;
-            }
+            UpdateLights(cycleTimer);
+            yield return null; // frame by frame for precision
+            cycleTimer += Time.deltaTime;
 
+            if (cycleTimer > totalCycle) cycleTimer = 0f;
+        }
+    }
+
+    void UpdateLights(float t)
+    {
+        // Light 1 timing
+        float redEnd = redTime;
+        float redYellowEnd = redEnd + redYellowTime;
+        float greenEnd = redYellowEnd + greenTime;
+        float yellowEnd = greenEnd + yellowTime;
+
+        // Wrap around
+        float localT1 = t % yellowEnd;
+
+        TrafficLightState state1;
+        if (localT1 < redEnd) state1 = TrafficLightState.Red;
+        else if (localT1 < redYellowEnd) state1 = TrafficLightState.RedYellow;
+        else if (localT1 < greenEnd) state1 = TrafficLightState.Green;
+        else state1 = TrafficLightState.Yellow;
+
+        // Light 2 is opposite phase (half a cycle offset)
+        float halfCycle = totalCycle / 2f;
+        float localT2 = (t + halfCycle) % totalCycle;
+
+        TrafficLightState state2;
+        if (localT2 < redEnd) state2 = TrafficLightState.Red;
+        else if (localT2 < redYellowEnd) state2 = TrafficLightState.RedYellow;
+        else if (localT2 < greenEnd) state2 = TrafficLightState.Green;
+        else state2 = TrafficLightState.Yellow;
+
+        ApplyState(state1, state2);
+    }
+
+  
+    void ApplyState(TrafficLightState s1, TrafficLightState s2)
+    {
+        foreach (var mat in mats1) mat.SetInt("_State", (int)s1);
+        foreach (var mat in mats2) mat.SetInt("_State", (int)s2);
+    }
+
+   /* IEnumerator LightStateSwitch (int lightIndex)
+    {
+        while (true)
+        {
+            float waitTime = StateSelect(lightIndex);
             yield return new WaitForSeconds(waitTime);
-            
-            IncrementState();
+            IncrementState(lightIndex);
             UpdateMaterialState();
             Debug.Log("Switching Traffic Lights" + currentState1 + " " + currentState2);
 
         }
-   }
+    }
+   */
+   
+
     public TrafficLightState CheckLightState1()
     {
         return currentState1;
@@ -77,12 +120,18 @@ public class TrafficLightManager : MonoBehaviour
         return currentState2;
     }
 
-   void IncrementState()
+   void IncrementState(int index)
     {
-        currentState1 += 1;
-        currentState2 += 1;
-        if ((int)currentState1 > 3) currentState1 = 0;
-        if ((int)currentState2 > 3) currentState2 = 0;
+        if (index == 1) {
+            currentState1 += 1;
+            if ((int)currentState1 > 3) currentState1 = 0;
+            return;
+        }
+        else if (index == 2) {
+            currentState2 += 1;
+            if ((int)currentState2 > 3) currentState2 = 0;
+            return;
+        }
     }
    void GetMaterials ()
     {
@@ -92,7 +141,7 @@ public class TrafficLightManager : MonoBehaviour
             var renderer = light.GetComponent<Renderer>();
             if (renderer != null)
             {
-                materialsLights1.Add(renderer.material);
+                mats1.Add(renderer.material);
             }
         }
         foreach (var light in trafficLights2)
@@ -100,21 +149,21 @@ public class TrafficLightManager : MonoBehaviour
             var renderer = light.GetComponent<Renderer>();
             if (renderer != null)
             {
-                materialsLights2.Add(renderer.material);
+                mats2.Add(renderer.material);
             }
         }
     }
 
    void UpdateMaterialState()
     {
-        foreach (var mat in materialsLights1)
+        foreach (var mat in mats1)
         {
             if (mat != null)
             {
                 mat.SetInt("_State", (int)currentState1);
             }
         }
-        foreach (var mat in materialsLights2)
+        foreach (var mat in mats2)
         {
             if (mat != null)
             {
