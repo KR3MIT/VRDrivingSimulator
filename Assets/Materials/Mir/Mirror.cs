4e5d3e8508727158cec5bcf62b937453a2a5b944
m4tt <33161422+m4ttxdd@users.noreply.github.com>
2025-10-09 14:29:25 +0200

    MIrror mirror on the mirror

---
+++ Assets/Materials/Mir/Mirror.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+//https://www.youtube.com/watch?v=_jBWm0KM2ec
+
+public class Mirror : MonoBehaviour
+{
+    public Transform playerCamera, mirrorCamera;
+
+    private void Update()
+    {
+        Vector3 posY = new Vector3(transform.position.x, playerCamera.position.y, transform.position.z);
+        Vector3 side1 = playerCamera.position - posY;
+        Vector3 side2 = transform.forward;
+        float angle = Vector3.SignedAngle(side1, side2, Vector3.up);
+
+        mirrorCamera.localEulerAngles = new Vector3(0, angle, 0);
+    }
+}
