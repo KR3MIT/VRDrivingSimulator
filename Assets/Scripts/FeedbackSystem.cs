de653b1035e320ad5febf1fd4b3528e877215fc9
m4tt <33161422+m4ttxdd@users.noreply.github.com>
2025-10-29 10:35:49 +0100

    Struct creation

--- Assets/Scripts/FeedbackSystem.cs
+++ Assets/Scripts/FeedbackSystem.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
+public struct DrivingError
+{
+    public string errorName;
+    public float timestamp;
+    public string description;
+    public ErrorSeverity severity;
+
+    public enum ErrorSeverity
+    {
+        Extreme,
+        High,
+        Medium,
+        Low,
+    }
+
+}
+
 public class FeedbackSystem : MonoBehaviour
 {
-
-    public string errorLogName = "errorLog.txt";
-
-    public TMP_Text feedbackText;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        string filepath = Path.Combine(Application.persistentDataPath, errorLogName);
-        Debug.Log(Application.persistentDataPath);
-
-        if (File.Exists(filepath))
-        {
-            string feedback = File.ReadAllText(filepath);
-            feedbackText.text = feedback;
-        } else
-        {
-            feedbackText.text = "No error log found.";
-        }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
