e7d21002ad0be538596b5ce6c2fa60531298190a
KR3MIT <jason.cm.hassan@gmail.com>
2025-12-11 11:26:43 +0100

    dolly things yaya

--- Assets/Scripts/AV/DollyPlay.cs
+++ Assets/Scripts/AV/DollyPlay.cs
@@ -6,8 +6,12 @@ using UnityEngine.InputSystem;
 public class DollyPlay : MonoBehaviour
 {
     private CinemachineSplineDolly dolly;
-    public float speed = 5f;
+    public float speed;
+    public bool dampening;
+    public float damp;
     public PlayerInput input;
+    private float velocity;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,13 +22,28 @@ public class DollyPlay : MonoBehaviour
 
     private IEnumerator PlayDolly()
     {
+        dolly.CameraPosition = 0f;
         float t = 0f;
+        float targetT = 1f;
+        velocity = 0f;
         var dollyLength = dolly.Spline.CalculateLength();
-        while (t < 1f)
+        
+        while (t < 1)
         {
-            t += Time.deltaTime * speed / dollyLength;
-            dolly.CameraPosition = Mathf.Clamp01(t);
+            if (!dampening) 
+            { 
+                t += Time.deltaTime * speed / dollyLength;
+                dolly.CameraPosition = Mathf.Clamp01(t);
+            }
+            else
+            {
+                t = Mathf.SmoothDamp(t, targetT, ref velocity, damp, speed / dollyLength);
+                dolly.CameraPosition = Mathf.Clamp01(t);
+            }
             yield return null;
         }
+        
+        // Ensure we reach the end
+        dolly.CameraPosition = 1f;
     }
 }
